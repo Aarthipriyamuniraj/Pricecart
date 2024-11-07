068b5af11bac77787649cea54f44e0808fcfae71
Aarthipriyamuniraj <aarthipriyamuniraj03@gmail.com>
2024-11-07 11:58:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Cartprice from './Components/Cartprice'
+Cartprice
+
+function App() {
+ let data= null;
+
+  return (
+      <>
+      <Cartprice data={data}/>
+      </>
+  )
+}
+
+export default App
